74d86222fe5d020fc4c18ff44718c1c5ed842472
Ta-Ye <cty1272@naver.com>
2021-02-05 17:46:25 +0900

    부트스트랩 템플릿 추가

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,35 +1,20 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <router-view name="header"></router-view>
+        <main>
+            <fade-transition origin="center" mode="out-in" :duration="250">
+                <router-view/>
+            </fade-transition>
+        </main>
+        <router-view name="footer"></router-view>
     </div>
-    <router-view/>
-  </div>
 </template>
-
 <script>
+import { FadeTransition } from "vue2-transitions";
+
+export default {
+  components: {
+    FadeTransition
+  }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
