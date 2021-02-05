7e136d848d7db9609d0bf59f4e6b903b3bfbbc99
Ta-Ye <cty1272@naver.com>
2021-02-05 15:03:57 +0900

    add project files

---
+++ src/main/java/io/gourmand/GourmandApplication.java
@@ -0,0 +1,13 @@
+package io.gourmand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/playdata/GourmandApplicationTests.java
@@ -0,0 +1,13 @@
+package io.playdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
