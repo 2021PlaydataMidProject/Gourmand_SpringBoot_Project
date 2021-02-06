edaafc7b72f190cf5aabf00414355cb3c463fab1
kowo1001 <kowo1001@dongguk.edu>
2021-02-06 20:26:12 +0900

    lombok Data 제거

--- src/main/java/io/gourmand/domain/Thread.java
+++ src/main/java/io/gourmand/domain/Thread.java
@@ -2,15 +2,16 @@ package io.gourmand.domain;
 
 import java.time.LocalDate;
 import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
