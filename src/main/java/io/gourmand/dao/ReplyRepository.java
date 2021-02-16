a584f11946833885474e97fab0420e9f344880ca
kowo1001 <kowo1001@dongguk.edu>
2021-02-16 10:25:41 +0900

    thread->reply로 수정

---
+++ src/main/java/io/gourmand/dao/ReplyRepository.java
@@ -0,0 +1,10 @@
+package io.gourmand.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.gourmand.domain.Reply;
+
+public interface ReplyRepository extends CrudRepository<Reply, Long> {
+
+}
+
