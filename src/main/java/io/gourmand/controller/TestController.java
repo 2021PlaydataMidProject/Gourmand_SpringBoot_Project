aebfdff9b0e1c433eaad65150b89f720ef39c5ba
kowo1001 <kowo1001@dongguk.edu>
2021-02-20 21:15:40 +0900

    jwt 사용 테스트

--- src/main/java/io/gourmand/controller/TestController.java
+++ src/main/java/io/gourmand/controller/TestController.java
@@ -3,6 +3,11 @@ package io.gourmand.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//JWT 토큰 안에 있는 값 가져올때 import
+import org.springframework.security.core.Authentication;
+import io.jsonwebtoken.Claims;
+
+
 @RestController
 public class TestController {
 	@GetMapping("/api/hello")
@@ -10,4 +15,26 @@ public class TestController {
 		System.out.println("ddd");
 		return "String";
 	}
+	
+	//JWT 토큰 테스트
+	@GetMapping("/user")
+	public String create(Authentication authentication) {
+		Claims claims = (Claims) authentication.getPrincipal();
+
+		String user_id = claims.get("user_id", String.class); // 토큰에 넣은 이름 적용 //user_id, user_num
+		
+		System.out.println("test " + user_id);
+
+		String url = "/user/" + user_id;
+		return url;
+	}
+	// Header에 아래 값 넣어주기
+	//	KEY : Authorization
+	//	VALUE : Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX251bSI6NCwidXNlcl9pZCI6InRlc3Q2IiwibmFtZSI6Imp3In0.9IZJIHCQ7jEjh31vZye5SZwzrnrWEkwLWHug2hDnoME
+	//	주의사항 : 'Bearer ' -> 토큰 앞에 꼭 붙여주기
+
+	
+	
+	
+	
 }
